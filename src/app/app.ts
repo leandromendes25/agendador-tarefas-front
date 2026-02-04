51cfd2890d4cdf94c07bb565f4a92341c8a0ab42
leandromendes25 <leandromiendes@hotmail.com>
2026-02-04 13:51:19 -0300

    create register component interface

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Register } from './pages/register/register';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('agendador-tarefas');

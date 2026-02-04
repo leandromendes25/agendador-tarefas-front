7e47bd5e5cc8b4e644cd5066157b31e93d56bdfa
leandromendes25 <leandromiendes@hotmail.com>
2026-02-04 15:05:20 -0300

    crate input password component

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -5,9 +5,17 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { NONE_TYPE } from '@angular/compiler';
+import { PasswordField } from '../../shared/components/password-field/password-field';
 @Component({
   selector: 'app-register',
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
+  imports: [
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    PasswordField,
+  ],
   templateUrl: './register.html',
   styleUrl: './register.scss',
   encapsulation: ViewEncapsulation.None,

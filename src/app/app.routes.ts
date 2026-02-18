b814cafe1d9e8ba6c9b45498ff820eebbcf72acf
leandromendes25 <leandromiendes@hotmail.com>
2026-02-18 15:55:05 -0300

    create home page and footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Register } from './pages/register/register';
 import { Login } from './pages/login/login';
+import { Home } from './pages/home/home';
 //caminho e o componente
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'register', component: Register },
   { path: 'login', component: Login },
 ];
